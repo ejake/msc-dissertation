8027fd68625a5b58763d7d2ab67f891880e54d7e
andres.jaque@gmail.com <andres.jaque@gmail.com@326b75c5-41e8-5e40-a0fe-a25fa275b828>
2014-12-02 16:36:34 +0000

    figures to results chapter

--- trunk/Final Document/Images/Chapter5/comparative_results.m
+++ trunk/Final Document/Images/Chapter5/comparative_results.m
@@ -1,4 +1,6 @@
 % Comparative results
+m = dlmread('/media/andres/DATA/Documents/Seminario de Investigacion/VRP/Outcomes/Consolidate outcomes - forecasting.csv',',',1,1);
+
 figure
 plot(m(:,1),m(:,5),'o',m(:,1),m(:,7),'*',m(:,1),m(:,9),'x')
 xlabel('n')
@@ -8,3 +10,43 @@ legend('rollout','ga','memetic','Location','northwest');
 figure
 boxplot(m(:,[5 7 9]),'labels',{'rollout','ga','memetic'});
 ylabel('Expected distance')
+
+
+%time
+figure
+stem(m(:,1),m(:,[6 8]))
+legend('ga','memetic','Location','northwest');
+xlabel('n')
+ylabel('Running time')
+set(gca,'XLim',[0 160]);
+
+
+%ra
+scatter3(m(:,4),m(:,1),m(:,2),m(:,3),m(:,5),'fill')
+zlabel('Q')
+ylabel('n')
+xlabel('time(s)')
+%size - range
+%color - expected distance
+
+%ga
+scatter3(m(:,6),m(:,1),m(:,2),m(:,3),m(:,7),'fill')
+zlabel('Q')
+ylabel('n')
+xlabel('time(s)')
+%size - range
+%color - expected distance
+
+%memetic
+scatter3(m(:,8),m(:,1),m(:,2),m(:,3),m(:,9),'fill')
+zlabel('Q')
+ylabel('n')
+xlabel('time(s)')
+%size - range
+%color - expected distance
+
+
+%size - expected distance
+%color - range
+
+
