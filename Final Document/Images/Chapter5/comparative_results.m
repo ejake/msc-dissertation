86c8f5542ad3965d528d47e95cde9c35ef398cb0
andres.jaque <andres.jaque@gmail.com>
2015-03-12 01:53:37 +0000

    Final version to library

--- Final Document/Images/Chapter5/comparative_results.m
+++ Final Document/Images/Chapter5/comparative_results.m
@@ -1,8 +1,9 @@
 % Comparative results
-m = dlmread('/media/andres/DATA/Documents/Seminario de Investigacion/VRP/Outcomes/Consolidate outcomes - forecasting.csv',',',1,1);
+%m = dlmread('/media/andres/DATA/Documents/Seminario de Investigacion/VRP/Outcomes/Consolidate outcomes - forecasting.csv',',',1,1);
+m = dlmread('/home/ajaque/Documents/Seminario de Investigacion/VRP/Outcomes/Consolidate outcomes - forecasting.csv',',',1,1);
 
 figure
-plot(m(:,1),m(:,5),'o',m(:,1),m(:,7),'*',m(:,1),m(:,9),'x')
+plot(m(:,1),m(:,5),'o',m(:,1),m(:,7),'+',m(:,1),m(:,9),'x')
 xlabel('n')
 ylabel('Expected distance')
 legend('rollout','ga','memetic','Location','northwest');
@@ -36,6 +37,13 @@ ylabel('n')
 xlabel('time(s)')
 %size - range
 %color - expected distance
+
+m = dlmread('/home/ajaque/Documents/Seminario de Investigacion/VRP/Outcomes/Consolidate outcomes - expdVs_ra.csv',',',1,1);
+ms = sort(m,1);
+plot([1:159],ms(:,4),'ro',[1:159],ms(:,6),'g*')
+legend('a priori tour','rollout','Location','northwest');
+xlabel('instance')
+ylabel('Expected distance')
 
 %ga
 scatter3(m(:,6),m(:,1),m(:,2),m(:,3),m(:,7),'fill')
